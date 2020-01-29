8ace17409f90676b2567af1b3154e872151e634c
christylaperriere98 <49654225+christylaperriere98@users.noreply.github.com>
2020-01-29 10:40:16 -0500

    commit changes

---
+++ labs/1/Adder.js
@@ -0,0 +1,16 @@
+//creating a class named Adder
+class Adder {
+    //this.props
+
+    //method sum() which returns the some of the props 
+    //"a" and "b" if they exist 
+    //undefined if "a" and "b" do not exist 
+
+    //method render()
+    //returning a string that incldues the sum of "a" and "b"
+    //string inside paragraph element 
+    //<p>The sum of 5 and 10 is 15</p>
+}
+
+//exporting information in class Adder
+module.exports = Adder; 
---
+++ labs/1/index.js
@@ -0,0 +1,8 @@
+//require Adder.js file
+const Adder = require("./Adder.js")
+
+//instance of class 
+class Instance extends Adder {
+   //setting "a" and "b" 
+   //printing results to the console
+}
