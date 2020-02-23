46dbbe842f7eeda1c5845f31778802d191b5cf25
christylaperriere98 <49654225+christylaperriere98@users.noreply.github.com>
2020-02-23 16:15:28 -0500

    Commit Lab 3

--- labs/3/src/App.js
+++ labs/3/src/App.js
@@ -1,12 +1,12 @@
-// Importing the Card class
+// Importing card 
 import React from 'react'
 import Card from './components/Card/index.js'
 
 // JSX
 function App () {
   return (
-    <div className="App">
-      <Card content='This is a card!'></Card>
+    <div className = 'App'>
+      <Card content = 'This is a card!'></Card>
     </div>
   )
 }
